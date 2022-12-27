ae96e964eb7b8992dc852d8f213be4421341540c
Akeem <akeem.mighty@gmail.com>
2022-12-26 22:18:59 -0500

    Set Initial Variables + Looped through Array

--- Solution.java
+++ Solution.java
@@ -1,5 +1,10 @@
 class Solution {
+    //This program creates a running sum of a 1d Array 
     public int[] runningSum(int[] nums) {
-        
+        int INITVAL = 0;
+        int prevSumCache = INITVAL;
+        for(int i = INITVAL; i < nums.length; i++) {
+            
+        }
     }
 }
